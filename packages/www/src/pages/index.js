be6bc21be5c134d430423b5277d1846e743874a6
Antti Salo <antti.salo@sc5.io>
2021-04-15 10:58:48 +0100

    Netlify identity

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,4 +1,22 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Container, Heading, Button, Flex } from "theme-ui";
+import netlifyIdentity from "netlify-identity-widget";
 
-export default props => (<div><h1>Home</h1></div>)
+const Home = (props) => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
 
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Here we go</Heading>
+        <Button sx={{ marginTop: 2 }} onClick={() => netlifyIdentity.open()}>
+          Log in
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Home;
